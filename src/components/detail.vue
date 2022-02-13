da6fcc81e0259f33984b9205509546da802372ca
santiphap372 <jaguar13738@gmail.com>
2022-02-13 21:29:08 +0700

    first commit

---
+++ src/components/detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-continer>
+      <div class="text--primary"><h5>{{ title }}</h5></div>
+      <div class="text--primary"><h5>{{ price }}</h5> </div>
+      <div class="text--primary"><h5>{{ Description }}</h5></div>
+  </v-continer>
+</template>
+<script>
+export default {
+  data() {
+    return {  
+      
+      Description: this.$route.params.description,
+      price: this.$route.params.price,
+      title: this.$route.params.title,
+    };
+  },
+};
+</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Shop from "../views/Shop.vue";
+import detail from "../components/detail.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -9,6 +10,18 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/Shop",
+    name: "Shop",
+    component: Shop,
+    children: [
+      {
+        path: "detail:id",
+        name: "detail",
+        component: detail,
+      },
+    ],
   },
   {
     path: "/about",
